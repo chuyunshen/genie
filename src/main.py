57ae868c59a673fafa18f2a61ad187e50096d3f9
nataliamoran <natalja.moran@gmail.com>
2019-12-31 11:00:37 -0500

    added a commented out draft of main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
-from fb_user import FBUser
+from tools import set_up_fbuser
 
 
 def main():
     pass
+    # # here is a draft of main
+    # fbuser = set_up_fbuser()
+    # menu = Menu(fbuser)
+    # while (True):
+    #     menu.print_menu_options()
+    #     menu.get_user_choice()
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
