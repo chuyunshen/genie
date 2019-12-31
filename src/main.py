bff725a9a0d82a12b9b34ccce17a59f84e19dfa4
chuyunshen <chuyunshen123@gmail.com>
2019-12-31 18:16:17 -0500

    Fill in menu and main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from tools import set_up_fbuser
-
+from fb_user import set_up_fbuser
+from datetime import datetime
+from menu import *
 
 """
 This file is the entry point for the program
@@ -7,13 +8,44 @@ This file is the entry point for the program
 
 
 def main():
-    pass
-    # # here is a draft of main
-    # fbuser = set_up_fbuser()
-    # menu = Menu(fbuser)
-    # while (True):
-    #     menu.print_menu_options()
-    #     menu.get_user_choice()
+    fb_user = set_up_fbuser()
+    today = datetime.today()
+    # Upon login, automatically update birthday calendar and send scheduled
+    # birthday messages
+    fb_user.update_birthday_calendar()
+    fb_user.send_all_scheduled_birthday_messages(today)
+    menu = Menu(fb_user.get_friend_dict())
+    while True:
+        main_menu_choice = menu.get_main_menu_choice()
+        friend_name = menu.get_friend_name()
+        friend_uids = get_uid_by_name(friend_name, fb_user.get_friend_dict())
+
+        if len(friend_uids) > 1:
+            friend_uid = menu.get_friend_selection()
+        else:
+            friend_uid = friend_uids[0]
+
+        if main_menu_choice == "edit":
+            birthday = menu.get_friend_birthday()
+            fb_user.add_friend_birthday(friend_uid, birthday)
+
+        elif main_menu_choice == "schedule":
+
+            birthday_message_type = menu.get_birthday_message_type()
+            if birthday_message_type == 'random':
+                random_birthday_message_type = \
+                    menu.get_random_birthday_message_type()
+                message = tools.select_random_wish(random_birthday_message_type)
+
+            else:
+                message = menu.get_drafted_birthday_message()
+
+            fb_user.schedule_birthday_message_for_uid(friend_uid, message)
+            print("Message scheduled!")
+
+        elif main_menu_choice == 'exit':
+            fb_user.save_calendar()
+            fb_user.logout()
 
 
 if __name__ == "__main__":
--- src/menu.py
+++ src/menu.py
@@ -1,14 +1,8 @@
 from PyInquirer import style_from_dict, Token, prompt
 from PyInquirer import Validator, ValidationError
 import datetime
-
-style = style_from_dict({
-    Token.QuestionMark: '#E91E63 bold',
-    Token.Selected: '#b16286 bold',
-    Token.Instruction: '',  # default
-    Token.Answer: '#83a598 bold',
-    Token.Question: '',
-})
+# from fb_user import get_uid_by_name
+from fb_user import *
 
 
 class DateValidator(Validator):
@@ -23,88 +17,136 @@ class DateValidator(Validator):
 
 class StringValidator(Validator):
     def validate(self, document):
-
         if not document.text or document.text.strip() == '':
             raise ValidationError(
                 message="Please enter a non-empty input.",
                 cursor_position=len(document.text))  # Move cursor to end
 
 
-friends_list = ['1', '2']  # somehow pass it into here.
-print('Welcome to Genie!')
-
-main_menu_question = [
-    {
-        'type': 'list',
-        'name': 'main_menu',
-        'message': 'What would you like to do?',
-        'choices': ["1) Edit a friend's birthday",
-                    "2) Schedule/Update a Facebook birthday message"],
-        'filter': lambda val: val[0]
-    }]
-
-friend_name_question = [
-    {
-        'type': 'input',
-        'name': 'friend_name',
-        'message': 'Please enter your friend\'s first and last name (e.g., '
-                   'Jane Doe)',
-        'validate': StringValidator
-    }]
-
-friend_selection_question = [
-    {
-        'type': 'list',
-        'name': 'friend_selection',
-        'message': 'There are multiple friends with the same name. '
-                   'Their Facebook profile pictures are included in the links.',
-        'choices': friends_list
-    }]
-
-friend_birthday_question = [
-    {
-        'type': 'input',
-        'name': 'friend_birthday',
-        'message': 'Please enter your friend\'s birthday, '
-                   'in the format of mm-dd',
-        'validate': DateValidator,
-    }]
-
-birthday_message_type_question = [
-    {
-        'type': 'list',
-        'name': 'birthday_message_type',
-        'message': 'How would you like to generate the birthday message?',
-        'choices': ["Generate a random birthday message",
-                    "Draft a birthday message"],
-    }]
-
-random_birthday_message_type_question = [
-    {
-        'type': 'list',
-        'name': 'random_birthday_message_type',
-        'message': 'Would you like your message to be serious or funny?',
-        'choices': ["Serious",
-                    "Funny"],
-        'filter': lambda val: val.lower()
-    }]
-
-draft_birthday_message_question = [
-    {
-        'type': 'input',
-        'name': 'drafted_birthday_message',
-        'message': 'Please draft your birthday message:',
-        'validate': StringValidator,
-    }]
+def validate_name(name, friend_list):
+    try:
+        get_uid_by_name(name, friend_list)
+    except NameError:
+        raise ValidationError(
+            message="You don't have a friend with this name",
+            cursor_position=len(name))  # Move cursor to end
+    if not name or name.strip() == '':
+        raise ValidationError(
+            message="Please enter a non-empty input.",
+            cursor_position=len(name))  # Move cursor to end
 
 
-def menu():
-    main_menu_choice = prompt(main_menu_question, style=style)
-    friend_name = prompt(friend_name_question, style=style)
-    friend_selection = prompt(friend_selection_question, style=style)
-    friend_birthday = prompt(friend_birthday_question, style=style)
-    birthday_message_type = prompt(birthday_message_type_question, style=style)
-    random_birthday_message_type = prompt(random_birthday_message_type_question,
-                                          style=style)
-    drafted_birthday_message = prompt(draft_birthday_message_question,
-                                      style=style)
+class Menu:
+    main_menu_question = [
+        {
+            'type': 'list',
+            'name': 'main_menu',
+            'message': 'What would you like to do?',
+            'choices': ["Edit a friend's birthday",
+                        "Schedule/Update a Facebook birthday message"],
+        }]
+
+    friend_birthday_question = [
+        {
+            'type': 'input',
+            'name': 'friend_birthday',
+            'message': 'Please enter your friend\'s birthday, '
+                       'in the format of mm-dd',
+            'validate': DateValidator,
+        }]
+
+    birthday_message_type_question = [
+        {
+            'type': 'list',
+            'name': 'birthday_message_type',
+            'message': 'How would you like to generate the birthday message?',
+            'choices': ["1) Generate a random birthday message",
+                        "2) Draft a birthday message"],
+        }]
+
+    random_birthday_message_type_question = [
+        {
+            'type': 'list',
+            'name': 'random_birthday_message_type',
+            'message': 'Would you like your message to be serious or funny?',
+            'choices': ["Serious",
+                        "Funny"],
+            'filter': lambda val: val.lower()
+        }]
+
+    draft_birthday_message_question = [
+        {
+            'type': 'input',
+            'name': 'drafted_birthday_message',
+            'message': 'Please draft your birthday message:',
+            'validate': StringValidator,
+        }]
+
+    style = style_from_dict({
+        Token.QuestionMark: '#E91E63 bold',
+        Token.Selected: '#b16286 bold',
+        Token.Instruction: '',  # default
+        Token.Answer: '#83a598 bold',
+        Token.Question: '',
+    })
+
+    def __init__(self, friend_list):
+        print("Welcome to Genie--your one-stop program for automated birthday"
+              "wishes.")
+        self.friend_list = friend_list
+        self.friend_selection_question = [
+            {
+                'type': 'list',
+                'name': 'friend_selection',
+                'message': 'There are multiple friends with the same name. '
+                           'Their Facebook profile pictures are included in '
+                           'the links.',
+                'choices': self.friend_list
+            }]
+
+        self.friend_name_question = [
+            {
+                'type': 'input',
+                'name': 'friend_name',
+                'message': "Please enter your friend's first and last name "
+                           "(e.g., Jane Doe)",
+                'validate': lambda text: validate_name(text, self.friend_list)
+            }]
+
+    def update_friend_list(self, friend_list):
+        self.friend_list = friend_list
+
+    def get_main_menu_choice(self):
+        answer = prompt(self.main_menu_question, style=self.style)['main_menu']
+        if answer == self.main_menu_question[0]['choices'][0]:
+            return "edit"
+        else:
+            return "schedule"
+
+    def get_friend_name(self):
+        return prompt(self.friend_name_question,
+                      style=self.style)['friend_name']
+
+    def get_friend_selection(self):
+        return prompt(self.friend_selection_question,
+                      style=self.style)['friend_selection']
+
+    def get_friend_birthday(self):
+        return prompt(self.friend_birthday_question,
+                      style=self.style)['friend_birthday']
+
+    def get_birthday_message_type(self):
+        answer = prompt(self.birthday_message_type_question,
+                        style=self.style)['birthday_message_type']
+        if answer == self.birthday_message_type_question[0]['choices'][0]:
+            return "random"
+        else:
+            return "drafted"
+
+    def get_random_birthday_message_type(self):
+        return prompt(self.random_birthday_message_type_question,
+                      style=self.style)['random_birthday_message_type']
+
+    def get_drafted_birthday_message(self):
+        return prompt(self.draft_birthday_message_question,
+                      style=self.style)['drafted_birthday_message']
