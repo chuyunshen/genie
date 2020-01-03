7c8d76734a7420fd54b095e2471b70fd0d1f3d61
nataliamoran <natalja.moran@gmail.com>
2020-01-03 13:21:29 -0500

    added config settings for updating .ics file path in fb2cal .ini

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,15 @@
-TEST = True
+import configparser
+
+TEST = False
 if TEST:
     import test.config_test as config
 else:
     from src import config
+
+# setup correct .ics file path in fb2cal .ini
+if True:
+    configuration = configparser.ConfigParser()
+    configuration.read(config.config_dir)
+    configuration.set("AUTH", "ics_file_path", config.ics_file_path)
+    with open(config.config_dir, "w") as f:
+        configuration.write(f)
